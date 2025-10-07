92233bbe21dbcbfb8435d0cca5092ca0c41cf1b1
Everton Luis <evertontst@gmail.com>
2025-10-06 22:39:41 -0300

    Added .env with apis URLS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -8,5 +9,12 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+
+  runtimeConfig: {
+    public: {
+      allLeaguesApi: process.env.NUXT_PUBLIC_ALL_LEAGUES_API || 'https://www.thesportsdb.com/api/v1/json/3/all_leagues.php',
+      badgeLookupApi: process.env.NUXT_PUBLIC_BADGE_LOOKUP_API || 'https://www.thesportsdb.com/api/v1/json/3/search_all_seasons.php'
+    }
+  }
 })
